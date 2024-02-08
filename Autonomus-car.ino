c38be15f715617b0a6c25e189be2dc7db680d54c
Rotem <rotem.kassif@gmail.com>
2024-02-08 12:32:48 +0200

    Finished?

--- Autonomus-car.ino
+++ Autonomus-car.ino
@@ -8,6 +8,10 @@
 
 #define echoPin 11 // attach pin D2 Arduino to pin Echo of HC-SR04
 #define trigPin 12//attach pin D3 Arduino to pin Trig of HC-SR04
+#define motor1pin1 2
+#define motor1pin2 3
+#define motor2pin1 4
+#define motor2pin2 5
 
 
 // defines variables
@@ -17,7 +21,12 @@ int distance; // variable for the distance measurement
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  pinMode(motor1pin1, OUTPUT);
+  pinMode(motor1pin2, OUTPUT);
+  pinMode(motor2pin1,  OUTPUT);
+  pinMode(motor2pin2, OUTPUT);
   Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
+  DriveForward();
 }
 void loop() {
   // Clears the trigPin condition
@@ -31,9 +40,38 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
   distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  if(distance <= 15){
+    DriveBackward();
+    delay(300);
+    TurnLeft();
+    delay(500);
+    DriveForward();
+  }
+  else{
+    //DriveForward();
+  }
   #pragma region Debugging
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
   #pragma endregion
+}
+
+void DriveForward(){
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+  digitalWrite(motor2pin1, HIGH);
+  digitalWrite(motor2pin2, LOW);
+}
+void DriveBackward(){
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, HIGH);
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, HIGH);
+}
+void TurnLeft(){
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, HIGH);
 }
